d699797814c37b6a207cbeabe403bcdc9cbeefcc
Sanek <djShtaket88@mail.ru>
2019-04-24 11:12:17 +0300

    fix redirect middleware

--- app/Http/Middleware/CheckRedirectDb.php
+++ app/Http/Middleware/CheckRedirectDb.php
@@ -30,7 +30,24 @@ class CheckRedirectDb
         $existedInDb = $this->dispatch(new GetRedirectByUriQuery($request->getRequestUri()));
 
         if ($existedInDb) {
-           return redirect($existedInDb->url_new, 301);
+            return redirect($existedInDb->url_new, 301);
+        }
+
+        if ((strpos($request->getPathInfo(), '//') !== false) || (substr($request->getPathInfo(), -1) == '/' && $request->getPathInfo() != '/')) {
+
+            $actualUrl = preg_replace("/\/+/","/", $request->getPathInfo());
+
+            if( substr($actualUrl, -1) == '/' ){
+                while( substr($actualUrl, -1) == '/' ){
+                    $actualUrl = substr($actualUrl, 0, -1);
+                }
+            }
+            return redirect($actualUrl, 301);
+        }
+
+        if (strstr($request->fullUrl(), 'index.')) {
+            $actualUrl = preg_replace('#index\.(\w)*(\/)?#', '', $request->fullUrl());
+            return redirect($actualUrl, 301);
         }
 
         return $next($request);
